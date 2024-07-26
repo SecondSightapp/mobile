6e866033e761c14384c1d9b2bf06cc63c997bd2c
Smyan Sengupta <senguptasmyan@gmail.com>
2024-07-26 01:08:39 -0400

    Added mood data definitions

--- lib/src/ui/screens/calendar.dart
+++ lib/src/ui/screens/calendar.dart
@@ -3,7 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:secondsight/src/ui/components/journal_card.dart';
 import 'package:secondsight/src/ui/screens/mood_picker.dart';
 import '../themes/source_colors.dart';
-import '../components/star.dart';
+import '../components/mood_star.dart';
 import '../components/calendar_week.dart';
 
 class Calendar extends StatelessWidget {
