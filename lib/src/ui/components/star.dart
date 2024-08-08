e3ddcd8817a6c85eb2341d434f174e62b47af7ef
Smyan Sengupta <senguptasmyan@gmail.com>
2024-08-08 03:25:02 -0400

    Added star faces

--- lib/src/ui/components/star.dart
+++ lib/src/ui/components/star.dart
@@ -2,28 +2,42 @@ import 'package:flutter/material.dart';
 import '../../data/moods.dart';
 import '../../constants.dart';
 
+final Map<String, String> moodStars = {
+  "angry": "assets/images/stars/angry_star.png",
+  "annoyed": "assets/images/stars/annoyed_star.png",
+  "calm": "assets/images/stars/calm_star.png",
+  "chill": "assets/images/stars/chill_star.png",
+  "cool": "assets/images/stars/cool_star.png",
+  "delighted": "assets/images/stars/delighted_star.png",
+  "disgusted": "assets/images/stars/disgusted_star.png",
+  "happy": "assets/images/stars/happy_star.png",
+  "naughty": "assets/images/stars/naughty_star.png",
+  "neutral": "assets/images/stars/neutral_star.png",
+  "sad": "assets/images/stars/sad_star.png",
+  "worried": "assets/images/stars/worried_star.png",
+};
+
 class Star extends StatelessWidget {
   final String mood;
+  final String image;
+  final bool isPoppable;
 
-  const Star({super.key, required this.mood});
+  Star({super.key, required this.mood, required this.isPoppable}) : image = moodStars[mood] ?? "assets/images/stars/neutral_star.png";
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    return InkWell(
       onTap: () {
         DateTime now = DateTime.now();
         moodState.addMoodData(DateTime.utc(now.year, now.month, now.day), mood);
+        if (isPoppable) {
+          Navigator.pop(context);
+        }
       },
-      child: Container(
+      child: Image.asset(
+        image,
         width: MediaQuery.of(context).size.width * 0.25,
         height: MediaQuery.of(context).size.height * 0.1,
-        decoration: ShapeDecoration(
-          color: moods[mood],
-          shape: const StarBorder(
-            innerRadiusRatio: 0.5,
-            pointRounding: 0.5,
-          ),
-        ),
       ),
     );
   }
