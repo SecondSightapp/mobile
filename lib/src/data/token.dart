6761704065c27c44e4503612cc6e51cee1d613aa
Vn0m <juanpapolo123@gmail.com>
2024-08-11 01:56:49 -0400

    do global variable for token

---
+++ lib/src/data/token.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class TokenService {
+  static String _token = '';
+
+  static Future<void> fetchToken() async {
+    final url = 'http://localhost:8080/authenticate';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        // token or empty string, we could do if string empty user cant log in to homepage maybe
+        _token = responseData['token'] ?? '';
+      } else {
+        throw Exception('failed :( with status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      throw Exception('error fetching the token: $error');
+    }
+  }
+
+  // getter for token
+  static String get token => _token;
+}
+
