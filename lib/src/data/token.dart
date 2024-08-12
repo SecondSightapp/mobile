9d2013f35c204cff722ce9ba379c5451b8ad613f
Vn0m <juanpapolo123@gmail.com>
2024-08-11 23:19:40 -0400

    add token

--- lib/src/data/token.dart
+++ lib/src/data/token.dart
@@ -2,10 +2,10 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class TokenService {
-  static String _token = '';
+  static String _token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqdWFucGFwb2xvMTIzQGdtYWlsLmNvbSIsImlhdCI6MTcyMzQzMjY5NCwiZXhwIjoxNzIzNDM0NDk0fQ.Z6nHdy56iHVVs9n3tVk28qbuDBUI4ILwwLvDotxNRKR_XTAwXpB97lNdTA6eajXDS-56vQ1J5JMYq8LAlxhkuw';
 
   static Future<void> fetchToken() async {
-    final url = 'http://localhost:8080/authenticate';
+    final url = 'https://secondsight-backend.onrender.com/authenticate';
 
     try {
       final response = await http.get(Uri.parse(url));
