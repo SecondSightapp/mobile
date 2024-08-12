8247c775a4acf1f06787693a66d4f99caee84e3c
Vn0m <juanpapolo123@gmail.com>
2024-08-11 23:20:36 -0400

    fix

--- lib/src/data/token.dart
+++ lib/src/data/token.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class TokenService {
-  static String _token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqdWFucGFwb2xvMTIzQGdtYWlsLmNvbSIsImlhdCI6MTcyMzQzMjY5NCwiZXhwIjoxNzIzNDM0NDk0fQ.Z6nHdy56iHVVs9n3tVk28qbuDBUI4ILwwLvDotxNRKR_XTAwXpB97lNdTA6eajXDS-56vQ1J5JMYq8LAlxhkuw';
+  static String _token = '';
 
   static Future<void> fetchToken() async {
     final url = 'https://secondsight-backend.onrender.com/authenticate';
